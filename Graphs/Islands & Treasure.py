081e23c14102fe241d57cf69961a26b635059cfe
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2026-01-19 23:48:14 +0530

    Update comment link formatting in Islands & Treasure.py

--- Graphs/Islands & Treasure.py
+++ Graphs/Islands & Treasure.py
@@ -1,6 +1,6 @@
 """
 # Problem: Islands and Treasure (Nearest Treasure Chest)
-# Link: [https://neetcode.io/problems/islands-and-treasure/question?list=neetcode150]
+# [Link](https://neetcode.io/problems/islands-and-treasure/question?list=neetcode150)
 #
 # Pattern: Multi-Source Breadth-First Search (BFS)
 #
